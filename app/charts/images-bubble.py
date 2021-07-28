5e345b9757b60054b4a5c09cbeb06440c7d143f6
Christopher Nuland <cnuland@localhost.localdomain>
2021-07-28 13:08:48 -0400

    add charts

---
+++ app/charts/images-bubble.py
@@ -0,0 +1,207 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+import requests
+import sys
+import json
+import re
+from collections import Counter
+from random import randint
+
+session = requests.Session()
+session.verify = False
+session.headers = {
+    'Accept': 'application/json',
+}
+
+def bash_command(cmd): 
+  ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,universal_newlines=True)
+  return ps.communicate()[0]
+
+token = bash_command("cat /var/run/secrets/kubernetes.io/serviceaccount/token")
+if token is not None:
+  session.headers['Authorization'] = 'Bearer {0}'.format(token)
+
+colors_ = lambda n: list(map(lambda i: "#" + "%06x" % randint(0, 0xFFFFFF),range(n)))
+images = []
+
+# URL Base
+base_url = "https://kubernetes.default.svc.cluster.local/api/v1"
+users_base_url = "https://kubernetes.default.svc.cluster.local/api/user.openshift.io/v1"
+namespace = bash_command("cat /var/run/secrets/kubernetes.io/serviceaccount/namespace")
+namespaces = session.get(base_url + "/namespaces")
+namespaces.raise_for_status()
+
+if namespaces.status_code != 200:
+  print("Failed to get Namespaces: {}".format(namespaces.status_code))
+  sys.exit(1)
+print("Checking labels in Groups:")
+for namespace in namespaces.json()["items"]:
+  namespace_name = namespace["metadata"]["name"]
+  if not namespace_name.startswith("openshift") and not namespace_name.startswith("kube") and not namespace_name.startswith("default"):
+    builds = session.get("https://kubernetes.default.svc.cluster.local/apis/build.openshift.io/v1/namespaces/{}/buildconfigs".format(namespace_name))
+    builds.raise_for_status()
+    if builds.status_code != 200:
+      print("Failed to get builds for namespace: {}".format(namespace_name))
+      continue
+    for build in builds.json()["items"]:
+      image_src = build["spec"]["strategy"]["dockerStrategy"]["from"]["name"]
+      image = re.search("^(?:(?=[^:\/]{1,253})(?!-)[a-zA-Z0-9-]{1,63}(?<!-)(?:\.(?!-)[a-zA-Z0-9-]{1,63}(?<!-))*(?::[0-9]{1,5})?/)?((?![._-])(?:[a-z0-9._-]*)(?<![._-])(?:/(?![._-])[a-z0-9._-]*(?<![._-]))*)(?::(?![.-])[a-zA-Z0-9_.-]{1,128})?$", image_src).group(1)
+      images.append(image)
+      
+count = Counter(images).most_common()
+browser_market_share = {
+    'browsers': [key for key, _ in count],
+    'market_share': [float(value) for _, value in count],
+    'color': colors_(len(count))
+}
+
+print(browser_market_share)
+class BubbleChart:
+    def __init__(self, area, bubble_spacing=0):
+        """
+        Setup for bubble collapse.
+
+        Parameters
+        ----------
+        area : array-like
+            Area of the bubbles.
+        bubble_spacing : float, default: 0
+            Minimal spacing between bubbles after collapsing.
+
+        Notes
+        -----
+        If "area" is sorted, the results might look weird.
+        """
+        area = np.asarray(area)
+        r = np.sqrt(area / np.pi)
+
+        self.bubble_spacing = bubble_spacing
+        self.bubbles = np.ones((len(area), 4))
+        self.bubbles[:, 2] = r
+        self.bubbles[:, 3] = area
+        self.maxstep = 2 * self.bubbles[:, 2].max() + self.bubble_spacing
+        self.step_dist = self.maxstep / 2
+
+        # calculate initial grid layout for bubbles
+        length = np.ceil(np.sqrt(len(self.bubbles)))
+        grid = np.arange(length) * self.maxstep
+        gx, gy = np.meshgrid(grid, grid)
+        self.bubbles[:, 0] = gx.flatten()[:len(self.bubbles)]
+        self.bubbles[:, 1] = gy.flatten()[:len(self.bubbles)]
+
+        self.com = self.center_of_mass()
+
+    def center_of_mass(self):
+        return np.average(
+            self.bubbles[:, :2], axis=0, weights=self.bubbles[:, 3]
+        )
+
+    def center_distance(self, bubble, bubbles):
+        return np.hypot(bubble[0] - bubbles[:, 0],
+                        bubble[1] - bubbles[:, 1])
+
+    def outline_distance(self, bubble, bubbles):
+        center_distance = self.center_distance(bubble, bubbles)
+        return center_distance - bubble[2] - \
+            bubbles[:, 2] - self.bubble_spacing
+
+    def check_collisions(self, bubble, bubbles):
+        distance = self.outline_distance(bubble, bubbles)
+        return len(distance[distance < 0])
+
+    def collides_with(self, bubble, bubbles):
+        distance = self.outline_distance(bubble, bubbles)
+        idx_min = np.argmin(distance)
+        return idx_min if type(idx_min) == np.ndarray else [idx_min]
+
+    def collapse(self, n_iterations=50):
+        """
+        Move bubbles to the center of mass.
+
+        Parameters
+        ----------
+        n_iterations : int, default: 50
+            Number of moves to perform.
+        """
+        for _i in range(n_iterations):
+            moves = 0
+            for i in range(len(self.bubbles)):
+                rest_bub = np.delete(self.bubbles, i, 0)
+                # try to move directly towards the center of mass
+                # direction vector from bubble to the center of mass
+                dir_vec = self.com - self.bubbles[i, :2]
+
+                # shorten direction vector to have length of 1
+                dir_vec = dir_vec / np.sqrt(dir_vec.dot(dir_vec))
+
+                # calculate new bubble position
+                new_point = self.bubbles[i, :2] + dir_vec * self.step_dist
+                new_bubble = np.append(new_point, self.bubbles[i, 2:4])
+
+                # check whether new bubble collides with other bubbles
+                if not self.check_collisions(new_bubble, rest_bub):
+                    self.bubbles[i, :] = new_bubble
+                    self.com = self.center_of_mass()
+                    moves += 1
+                else:
+                    # try to move around a bubble that you collide with
+                    # find colliding bubble
+                    for colliding in self.collides_with(new_bubble, rest_bub):
+                        # calculate direction vector
+                        dir_vec = rest_bub[colliding, :2] - self.bubbles[i, :2]
+                        dir_vec = dir_vec / np.sqrt(dir_vec.dot(dir_vec))
+                        # calculate orthogonal vector
+                        orth = np.array([dir_vec[1], -dir_vec[0]])
+                        # test which direction to go
+                        new_point1 = (self.bubbles[i, :2] + orth *
+                                      self.step_dist)
+                        new_point2 = (self.bubbles[i, :2] - orth *
+                                      self.step_dist)
+                        dist1 = self.center_distance(
+                            self.com, np.array([new_point1]))
+                        dist2 = self.center_distance(
+                            self.com, np.array([new_point2]))
+                        new_point = new_point1 if dist1 < dist2 else new_point2
+                        new_bubble = np.append(new_point, self.bubbles[i, 2:4])
+                        if not self.check_collisions(new_bubble, rest_bub):
+                            self.bubbles[i, :] = new_bubble
+                            self.com = self.center_of_mass()
+
+            if moves / len(self.bubbles) < 0.1:
+                self.step_dist = self.step_dist / 2
+
+    def plot(self, ax, labels, colors):
+        """
+        Draw the bubble plot.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes.Axes
+        labels : lists
+            Labels of the bubbles.
+        colors : list
+            Colors of the bubbles.
+        """
+        for i in range(len(self.bubbles)):
+            circ = plt.Circle(
+                self.bubbles[i, :2], self.bubbles[i, 2], color=colors[i])
+            ax.add_patch(circ)
+            ax.text(*self.bubbles[i, :2], labels[i] + "\n" + str(int(browser_market_share['market_share'][i])) ,
+                    horizontalalignment='center', verticalalignment='center')
+
+
+bubble_chart = BubbleChart(area=browser_market_share['market_share'],
+                           bubble_spacing=0.1)
+
+bubble_chart.collapse()
+
+fig, ax = plt.subplots(subplot_kw=dict(aspect="equal"))
+bubble_chart.plot(
+    ax, browser_market_share['browsers'], browser_market_share['color'])
+ax.axis("off")
+ax.relim()
+ax.autoscale_view()
+ax.set_title('Base Images')
+
+plt.savefig('/app/serve/images-bubble.png')
---
+++ app/charts/images-pi.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+import requests
+import sys
+import json
+import re
+from collections import Counter
+from random import randint
+
+session = requests.Session()
+session.verify = False
+session.headers = {
+    'Accept': 'application/json',
+}
+
+def bash_command(cmd): 
+  ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,universal_newlines=True)
+  return ps.communicate()[0]
+
+token = bash_command("cat /var/run/secrets/kubernetes.io/serviceaccount/token")
+if token is not None:
+  session.headers['Authorization'] = 'Bearer {0}'.format(token)
+
+colors_ = lambda n: list(map(lambda i: "#" + "%06x" % randint(0, 0xFFFFFF),range(n)))
+images = []
+
+# URL Base
+base_url = "https://kubernetes.default.svc.cluster.local/api/v1"
+users_base_url = "https://kubernetes.default.svc.cluster.local/api/user.openshift.io/v1"
+namespace = bash_command("cat /var/run/secrets/kubernetes.io/serviceaccount/namespace")
+namespaces = session.get(base_url + "/namespaces")
+namespaces.raise_for_status()
+
+if namespaces.status_code != 200:
+  print("Failed to get Namespaces: {}".format(namespaces.status_code))
+  sys.exit(1)
+print("Checking labels in Groups:")
+for namespace in namespaces.json()["items"]:
+  namespace_name = namespace["metadata"]["name"]
+  if not namespace_name.startswith("openshift") and not namespace_name.startswith("kube") and not namespace_name.startswith("default"):
+    builds = session.get("https://kubernetes.default.svc.cluster.local/apis/build.openshift.io/v1/namespaces/{}/buildconfigs".format(namespace_name))
+    builds.raise_for_status()
+    if builds.status_code != 200:
+      print("Failed to get builds for namespace: {}".format(namespace_name))
+      continue
+    for build in builds.json()["items"]:
+      image_src = build["spec"]["strategy"]["dockerStrategy"]["from"]["name"]
+      image = re.search("^(?:(?=[^:\/]{1,253})(?!-)[a-zA-Z0-9-]{1,63}(?<!-)(?:\.(?!-)[a-zA-Z0-9-]{1,63}(?<!-))*(?::[0-9]{1,5})?/)?((?![._-])(?:[a-z0-9._-]*)(?<![._-])(?:/(?![._-])[a-z0-9._-]*(?<![._-]))*)(?::(?![.-])[a-zA-Z0-9_.-]{1,128})?$", image_src).group(1)
+      images.append(image)
+      
+count = Counter(images).most_common()
+browser_market_share = {
+    'browsers': [key for key, _ in count],
+    'market_share': [float(value) for _, value in count],
+    'color': colors_(len(count))
+}
+
+
+# Creating plot
+fig = plt.figure(figsize =(10, 7))
+plt.pie(browser_market_share["market_share"], labels = browser_market_share["browsers"])
+
+plt.savefig('/app/serve/image-pi.png')
